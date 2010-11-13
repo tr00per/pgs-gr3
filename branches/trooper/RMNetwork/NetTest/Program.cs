288ef9ea5d8d35047207fef552dd780895982ea3
marek.ropenga <marek.ropenga@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-13 16:05:55 +0000

    model dla sieci

---
+++ branches/trooper/RMNetwork/NetTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServerLogic sl = new ServerLogic();
+            ClientLogic cl = new ClientLogic();
+
+            sl.server.startServer();
+            Console.WriteLine("SERVER RUNNING: " + sl.server.isRunning().ToString());
+            cl.connect("127.0.0.1", 20000, "tr00per", 1);
+            Console.WriteLine("CLIENT RUNNING: " + cl.isRunning().ToString());
+            Console.WriteLine("WAITING...");
+            Thread.Sleep(1000);
+            Console.WriteLine("SENDING MSG...");
+            sl.spam();
+            Console.WriteLine("DONE.");
+            Console.WriteLine("SENDING MSG...");
+            sl.spam();
+            Console.WriteLine("DONE.");
+            Console.WriteLine("DOING RADNOM STUFF...");
+            for (int i = 0; i < 100; ++i)
+            {
+                int c = i * 2 / 4;
+                Thread.Sleep(Math.Max(c, 5));
+            }
+            Console.WriteLine("DONE.");
+            cl.disconnect();
+            Console.WriteLine("CLIENT RUNNING: " + cl.isRunning().ToString());
+            sl.server.stopServer();
+            Console.WriteLine("SERVER RUNNING: " + sl.server.isRunning().ToString());
+
+            Console.WriteLine("DONE!");
+        }
+    }
+}
---
+++ branches/trooper/RMNetwork/Network/Common.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Network
+{
+    abstract public class Common
+    {
+        public static byte PACKET_OK = 1;
+        public static byte PACKET_FAIL = 2;
+        public static byte PACKET_HANDSHAKE = 4;
+        public static byte PACKET_BEGIN = 8;
+        public static byte PACKET_COMMON = 16;
+        public static byte PACKET_END = 32;
+        public static byte PACKET_SRVMSG = 64;
+
+        public static int PACKET_HEADER_SIZE = 2;
+
+        public static bool correctPacket(byte[] data, byte packetType)
+        {
+            return (data[0] & packetType) > 0 && (data[1] == checksum(data));
+        }
+
+        public static bool correctPacket(byte[] data, int packetType)
+        {
+            return correctPacket(data, (byte)packetType);
+        }
+
+        public static byte checksum(byte[] data)
+        {
+            //skip header (packet type and checksum field)
+            return rawChecksum(data, Common.PACKET_HEADER_SIZE);
+        }
+
+        internal static byte rawChecksum(byte[] data, int offset)
+        {
+            uint cs = 0;
+            IEnumerable<byte> bytes = data.Skip(offset);
+            foreach (byte b in bytes)
+            {
+                cs += b;
+            }
+
+            return (byte)(cs & 255);
+        }
+
+        internal static void asyncWrite(IAsyncResult arg)
+        {
+            NetworkStream io = (NetworkStream)arg.AsyncState;
+            io.EndWrite(arg);
+        }
+    }
+}
