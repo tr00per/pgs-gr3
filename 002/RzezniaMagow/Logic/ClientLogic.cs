8ee11215228a2831b062c0753c66a73e8a36e0e8
lachu87pl <lachu87pl@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-15 17:29:54 +0000

    Branch created

---
+++ 002/RzezniaMagow/Logic/ClientLogic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Network;
+
+namespace NetTest
+{
+    public class ClientLogic: Client
+    {
+        public ClientLogic()
+            : base(status)
+        {
+        }
+
+        override protected void updateArrived(byte[] data)
+        {
+        }
+
+        protected override void beginRound(byte[] data)
+        {
+        }
+
+        public static void status(string msg)
+        {
+            Console.WriteLine("Client: " + msg);
+        }
+    }
+}
