2c39863fa5e3d682d70384444214d2294eb123ec
arturczajka@gmail.com <arturczajka@gmail.com@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-15 14:19:02 +0000

    [branch:tr00per] mój branch trunka

---
+++ 002/RzezniaMagow/SytemIO/Mysz.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RzezniaMagow
+{
+
+    public class Myszka
+    {
+
+
+
+        MouseState originalMouseState;
+        
+        
+
+
+        public Myszka()
+        {
+            originalMouseState = Mouse.GetState();
+        }
+
+        /// <summary>
+        /// Funkcja realizacje działania przycisków i ruchu myszy.
+        /// </summary>
+        public void procesMyszy()
+        {
+            
+
+                    MouseState currentMouseState = Mouse.GetState();
+
+           
+            if(currentMouseState.LeftButton != originalMouseState.LeftButton)
+                if (currentMouseState.LeftButton == ButtonState.Pressed)
+                    {
+                        System.Console.WriteLine("strzal z myszki");
+                        Game.zawodnik.getPozycjaKursora = new Vector2(currentMouseState.X + Game.kamera.getPozycja.X - Game.graphics.PreferredBackBufferWidth / 2, currentMouseState.Y + Game.kamera.getPozycja.Y - Game.graphics.PreferredBackBufferHeight / 2);
+                       //System.Console.WriteLine(currentMouseState.X + "    " + currentMouseState.Y); 
+                    }
+
+
+                    if (currentMouseState.X != originalMouseState.X)
+                    {
+                        if (currentMouseState.X > 0 && currentMouseState.X < 800 && currentMouseState.Y > 0 && currentMouseState.Y < 600)
+
+                           // Game.zawodnik.getPozycjaKursora = new Vector2(currentMouseState.X, currentMouseState.Y);
+                        System.Console.WriteLine(currentMouseState.X+"    "+ currentMouseState.Y); 
+                        
+                    }
+
+                
+                   
+                    this.originalMouseState = Mouse.GetState();
+                   
+                
+            
+        }
+
+    }
+}
