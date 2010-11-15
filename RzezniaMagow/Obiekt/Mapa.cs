e8e6a990d1bb1e8b77242250ba20ddaf3e77d368
gd8700 <gd8700@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-15 13:57:21 +0000

    pierwszy trunk

---
+++ RzezniaMagow/Obiekt/Mapa.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RzezniaMagow
+{
+    public class Mapa : Obiekt
+    {
+        private Vector2 pozycja;
+
+        private String nazwaPliku;
+        
+        
+
+
+        public Mapa(float x, float y): base(x, y)
+        {
+            
+            
+        }
+
+        public override void LoadContent(Texture2D tlo)
+        {
+            base.LoadContent(tlo);
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+
+            base.Draw(gameTime, spriteBatch);
+
+
+
+
+        }
+        
+
+       
+        
+        public String getNazwaPliku
+        {
+            get { return nazwaPliku; }
+            set { nazwaPliku = value; }
+        }
+
+        public Vector2 getPozycja
+        {
+            get { return pozycja; }
+            set { pozycja = value; }
+        }
+
+
+
+
+
+
+
+    }
+}
