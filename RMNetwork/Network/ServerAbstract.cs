389221a59d8b016c248221b622b5bc3d619c4bd6
arturczajka@gmail.com <arturczajka@gmail.com@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-15 13:19:51 +0000

    [branch:tr00per] Po próbnej integracji  - All: dodana dokumentacja (praca w toku)  - Client, Server: klient i server są używalni po rozłączeniu, wcześniej trzeba by od nowa tworzyć obiekt  - Client: obsługa pakietu początku rundy (+ zwrotka)  - Server: rozłączanie klientów przy wyłączaniu serwera  - Client: obsługa wyłącznia serwera  - ClientLogic, ServerLogic: są publiczne, ale to do testów

--- RMNetwork/Network/ServerAbstract.cs
+++ RMNetwork/Network/ServerAbstract.cs
@@ -9,20 +9,49 @@ namespace Network
     {
         private Server srv;
 
+        /// <summary>
+        /// Give server, which has instance of ServerAbstract given, to this instance.
+        /// </summary>
+        /// <param name="server">Initialized server.</param>
         protected void bindServer(Server server)
         {
             srv = server;
         }
 
+        /// <summary>
+        /// Handshake callback.
+        /// </summary>
+        /// <param name="nick">New player's nick.</param>
+        /// <param name="avatar">New player's avatar.</param>
+        /// <returns>Unique player ID.</returns>
         abstract protected internal byte newPlayerConnected(string nick, byte avatar);
+
+        /// <summary>
+        /// Common communication callback. Run asynchronously.
+        /// </summary>
+        /// <param name="data">byte[] for deserialization.</param>
         abstract protected internal void playerHandle(object data);
+
+        /// <summary>
+        /// Player said goodbye.
+        /// </summary>
+        /// <param name="id">ID of disconnecting client.</param>
         abstract protected internal void playerParted(byte id);
 
+        /// <summary>
+        /// Common comunication update.
+        /// This should be run periodically to provide synchronization.
+        /// </summary>
+        /// <param name="data">Content.</param>
         protected void sendUpdate(byte[] data)
         {
             srv.broadcast(Common.PACKET_COMMON, data);
         }
 
+        /// <summary>
+        /// Send broadcast text message to everyone.
+        /// </summary>
+        /// <param name="msg">Message, max 200 chars.</param>
         protected internal void sendMessage(string msg)
         {
             if (msg.Length > 200)
