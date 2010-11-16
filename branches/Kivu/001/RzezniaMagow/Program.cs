f511e98d907a30ffd6a5585ab6735c191e3a3ffc
gd8700 <gd8700@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-16 12:30:26 +0000

    dodane klasy do tworzenia gry i główne menu

--- branches/Kivu/001/RzezniaMagow/Program.cs
+++ branches/Kivu/001/RzezniaMagow/Program.cs
@@ -4,15 +4,18 @@ namespace RzezniaMagow
 {
     static class Program
     {
+        public static Game game;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
         static void Main(string[] args)
         {
-            using (Game game = new Game())
-            {
+            game = new Game();
                 game.Run();
-            }
+
+
         }
     }
 }
