e64cb6c895760908b51cc8b92d18de59949e76d0
arturczajka@gmail.com <arturczajka@gmail.com@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-17 11:02:04 +0000

    [branch:tr00per] część logiki serwera  - Gracz: nowy konstuktor - przyjmujący tylko ID, który nie jest ściągany z klasy Obiekt  - ServerAbstract: dwa nowe "eventy" dla serwera, zmiana widoczności starych, zmiana wywołania sendMessage()  - Server: dwa nowe "eventy", komentarze  - ServerLogic: wszystko poza wpisywaniem danych otrzymanych od gracza (wysyła update'y) i rozpoczynaniem rundy

--- 002/RzezniaMagow/Logic/ServerLogic.cs
+++ 002/RzezniaMagow/Logic/ServerLogic.cs
@@ -3,41 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Network;
+using System.Threading;
+using System.Timers;
 
-namespace Logic
+namespace RzezniaMagow
 {
     public class ServerLogic: Network.ServerAbstract
     {
-        public Server server;
+        private Server server;
+
+        private List<Gracz> players;
+        private List<Pocisk> bullets;
+        private byte nextPlayerID;
+
+        private byte roundNumber;
+
+        private System.Timers.Timer updateTimer;
+
+        private SerwerProtocol prot;
 
         public ServerLogic()
         {
+            roundNumber = 0;
+            nextPlayerID = 0;
+            prot = new SerwerProtocol();
             server = new Server(this, 20000, 5);
             bindServer(server);
+            updateTimer = new System.Timers.Timer(30);
+            updateTimer.Elapsed += new ElapsedEventHandler(updateTimerCB);
         }
 
-        override protected internal byte newPlayerConnected(string nick, byte avatar)
+        public override void serverStarted()
         {
-            //add player to list, return his uniqe id
-            return 0;
+            updateTimer.Start();
         }
 
-        override protected internal void playerHandle(object data)
+        public override void serverStopped()
+        {
+            updateTimer.Stop();
+        }
+
+        /// <summary>
+        /// New player connected. Give him his ID.
+        /// </summary>
+        public override byte newPlayerConnected(string nick, byte avatar)
+        {
+            Gracz p = new Gracz(nextPlayerID);
+            p.getNick = nick;
+            p.getTypAvatara = avatar;
+            players.Add(p);
+            return nextPlayerID++;
+        }
+
+        private void updateTimerCB(object o, ElapsedEventArgs args)
+        {
+            prot.createPackage(players, bullets, Common.PACKET_COMMON, roundNumber); //FIXME change arguments
+            byte[] data = prot.getTablica;
+            sendUpdate(Common.PACKET_COMMON, data);
+        }
+
+        public override void playerHandle(object data)
         {
             byte[] d = (byte[])data;
             byte id = d[0];
 			Console.WriteLine(id + " send something to server.");
         }
 
-        override protected internal void playerParted(byte id)
+        volatile byte _removeID;
+
+        public override void playerParted(byte id)
         {
-            //remove player from list
-            //release his id for future use (with nonzero retention time)
+            _removeID = id;
+            players.Remove(players.Find(new Predicate<Gracz>(playerRemover)));
         }
 
-        public void spam()
+        private bool playerRemover(Gracz p)
         {
-            sendMessage("SPAM, SPAM, SPAM, SPAM, SPAM...");
+            return p.getID == _removeID;
         }
     }
 }
--- 002/RzezniaMagow/Network/ServerAbstract.cs
+++ 002/RzezniaMagow/Network/ServerAbstract.cs
@@ -19,40 +19,50 @@ namespace Network
         }
 
         /// <summary>
+        /// Invoked when server has started.
+        /// </summary>
+        abstract public void serverStarted();
+
+        /// <summary>
+        /// Invoked when server is stopping.
+        /// </summary>
+        abstract public void serverStopped();
+
+        /// <summary>
         /// Handshake callback.
         /// </summary>
         /// <param name="nick">New player's nick.</param>
         /// <param name="avatar">New player's avatar.</param>
         /// <returns>Unique player ID.</returns>
-        abstract protected internal byte newPlayerConnected(string nick, byte avatar);
+        abstract public byte newPlayerConnected(string nick, byte avatar);
 
         /// <summary>
         /// Common communication callback. Run asynchronously.
         /// </summary>
         /// <param name="data">byte[] for deserialization.</param>
-        abstract protected internal void playerHandle(object data);
+        abstract public void playerHandle(object data);
 
         /// <summary>
         /// Player said goodbye.
         /// </summary>
         /// <param name="id">ID of disconnecting client.</param>
-        abstract protected internal void playerParted(byte id);
+        abstract public void playerParted(byte id);
 
         /// <summary>
         /// Common comunication update.
         /// This should be run periodically to provide synchronization.
         /// </summary>
         /// <param name="data">Content.</param>
-        protected void sendUpdate(byte[] data)
+        public void sendUpdate(byte type, byte[] data)
         {
-            srv.broadcast(Common.PACKET_COMMON, data);
+            srv.broadcast(type, data);
         }
 
         /// <summary>
         /// Send broadcast text message to everyone.
         /// </summary>
         /// <param name="msg">Message, max 200 chars.</param>
-        protected internal void sendMessage(string msg)
+        public void sendMessage(string msg)
         {
             if (msg.Length > 200)
             {
