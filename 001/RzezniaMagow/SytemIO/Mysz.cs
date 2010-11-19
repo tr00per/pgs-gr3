dd2cb437608b08c22ac1f9af7108217e480d8217
gd8700 <gd8700@b931f6cd-0e61-0a53-adfe-06e1e69e9a4c>
2010-11-19 14:23:23 +0000

    poprawiona obsługa myszki

--- 001/RzezniaMagow/SytemIO/Mysz.cs
+++ 001/RzezniaMagow/SytemIO/Mysz.cs
@@ -37,6 +37,7 @@ namespace RzezniaMagow
 
                         if (currentMouseState.LeftButton != originalMouseState.LeftButton)
                             if (currentMouseState.LeftButton == ButtonState.Pressed)
+                                if (currentMouseState.X > 0 && currentMouseState.X < 800 && currentMouseState.Y > 0 && currentMouseState.Y < 600)
                             {
                                 System.Console.WriteLine("strzal z myszki");
                                 Game.zawodnik.getPozycjaKursora = new Vector2(currentMouseState.X + Game.kamera.getPozycja.X - Game.graphics.PreferredBackBufferWidth / 2, currentMouseState.Y + Game.kamera.getPozycja.Y - Game.graphics.PreferredBackBufferHeight / 2);
@@ -46,13 +47,13 @@ namespace RzezniaMagow
                             }
 
 
-                        if (currentMouseState.X != originalMouseState.X)
-                        {
+                        //if (currentMouseState.X != originalMouseState.X)
+                        //{
                             if (currentMouseState.X > 0 && currentMouseState.X < 800 && currentMouseState.Y > 0 && currentMouseState.Y < 600)
                             Game.zawodnik.getPozycjaKursora = new Vector2(currentMouseState.X + Game.kamera.getPozycja.X - Game.graphics.PreferredBackBufferWidth / 2, currentMouseState.Y + Game.kamera.getPozycja.Y - Game.graphics.PreferredBackBufferHeight / 2);
 
-                        }
-
+                       // }
+                        //System.Console.WriteLine("pozycja myszki X: " + currentMouseState.X + " pozycja myszki Y: " + currentMouseState.X);
 
 
                         this.originalMouseState = Mouse.GetState();
